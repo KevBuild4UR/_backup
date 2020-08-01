18b12e74f591c59c77fff269c80903ae64cde3d8
KevBuild4UR <68817111+KevBuild4UR@users.noreply.github.com>
2020-08-01 13:49:45 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -37,64 +37,7 @@ docute.init({
             tags: ['english', 'zh'],
             url: 'https://www.neptune.work'
         }),
-        player()
     ]
 });
 
-function player () {
-    return function (context) {
-        context.event.on('landing:updated', function () {
-            console.log('landing:updated');
-            clearPlayer();
-            aplayer0();
-            aplayer1();
-        });
-        context.event.on('content:updated', function () {
-            console.log('content:updated');
-            clearPlayer();
-            for (let i = 0; i < document.querySelectorAll('.load').length; i++) {
-                document.querySelectorAll('.load')[i].addEventListener('click', function () {
-                    window[this.parentElement.id] && window[this.parentElement.id]();
-                });
-            }
-        });
-    };
-}
 
-function clearPlayer () {
-    for (let i = 0; i < 10; i++) {
-        if (window['ap' + i]) {
-            window['ap' + i].destroy();
-        }
-    }
-}
-
-function aplayerfixed () {
-    window.ap1 = new APlayer({
-        container: document.getElementById('aplayerfixed'),
-        theme: '#007BFF',
-        fixed: true,
-        audio: [{
-            name: '光るなら',
-            artist: 'Goose house',
-            url: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/hikarunara.mp3',
-            cover: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/hikarunara.jpg',
-            lrc: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/hikarunara.lrc',
-            theme: '#ebd0c2'
-        }, {
-            name: 'トリカゴ',
-            artist: 'XX:me',
-            url: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/darling.mp3',
-            cover: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/darling.jpg',
-            lrc: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/darling.lrc',
-            theme: '#46718b'
-        }, {
-            name: '前前前世',
-            artist: 'RADWIMPS',
-            url: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/yourname.mp3',
-            cover: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/yourname.jpg',
-            lrc: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/yourname.lrc',
-            theme: '#505d6b'
-        }]
-    });
-}
