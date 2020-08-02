65b651c3b5cc30a5c78697e995f065e904117183
KevBuild4UR <68817111+KevBuild4UR@users.noreply.github.com>
2020-08-02 10:02:11 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -29,7 +29,7 @@ docute.init({
                 title: '贡献者', path: '/zh/contributors'
             },
             {
-                title: '可用物品放置场景', path: '/ipls'               
+                title: '可用物品放置场景', path: '/zh/ipls'               
             },
             {
                 title: '语言', type: 'dropdown', items: langs
