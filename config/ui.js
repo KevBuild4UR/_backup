6fea45b44c87bb501b6eba7ecad4b4732a19f462
KevBuild4UR <68817111+KevBuild4UR@users.noreply.github.com>
2020-08-28 20:22:57 +0800

    Update ui.js

--- config/ui.js
+++ config/ui.js
@@ -11,8 +11,8 @@ var url = window.location.href;
 
 function getCaption(obj)
 {
-	var index = obj.lastIndexOf("\status/");
-	obj = obj.substring(index + 7, obj.length);
+	var index = obj.lastIndexOf("\/status/");
+	obj = obj.substring(index + 8, obj.length);
 	//  console.log(obj);
 	return obj;
 }
